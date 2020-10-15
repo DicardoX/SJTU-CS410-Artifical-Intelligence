efb051fe59f7dcd6ace00ce247104d726e754515
Dicardo Xue <49444151+DicardoX@users.noreply.github.com>
2020-10-15 20:52:46 +0800

    Create CNN_DEMO.py

---
+++ Project_1/CNN_DEMO.py
@@ -0,0 +1,40 @@
+############################# Obtain File List ##########################################
+
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+
+
+############################# Environment Settings ##########################################
+
+!pip install tensorflow==1.13.1 # 在使用pip指令前要加!
+
+import os
+import numpy as np
+# import pandas as pd
+import tensorflow as tf
+
+LR = 0.01
+BatchSize = 50
+EPOCH = 2
+print(tf.__version__)
+
+
+############################# Environment Settings ##########################################
+
+
