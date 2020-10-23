dfc5d031288ec67d6c38c753c1a1a3d737d34cb8
Dicardo Xue <49444151+DicardoX@users.noreply.github.com>
2020-10-23 16:40:18 +0800

    Update train.py

--- Project_1/train.py
+++ Project_1/train.py
@@ -9,9 +9,9 @@ import numpy as np
 import tensorflow as tf
 import time
 
-LR = 0.007
-BatchSize = 72
-EPOCH = 10
+LR = 0.01
+BatchSize = 64
+EPOCH = 5
 print(tf.__version__)
 
 ## Data Loading ##
@@ -45,7 +45,7 @@ def load_data(filefolder):
     
 ## Network Building ##
 
-def net(onehots_shape): #[73,398]
+def net(onehots_shape, lamda = 0.5): #[73,398]
     #if not isinstance(onehots_shape, list):
     onehots_shape = list(onehots_shape)
     input = tf.placeholder(tf.float32, [None] + onehots_shape, name='input') # tf.placeholder(): TensorFlow中的占位符，用于传入外部数据
@@ -69,7 +69,8 @@ def net(onehots_shape): #[73,398]
     output2 = tf.keras.layers.Dense(16, name='output2')(output1)
     output = tf.keras.layers.Dense(2, name='output')(output2)
     
-    loss = tf.losses.softmax_cross_entropy(onehot_labels=label, logits=output) # 计算cost
+    sigmoids = tf.nn.sigmoid_cross_entropy_with_logits(labels=label, logits=output) 
+    loss = tf.reduce_mean(sigmoids) #loss_collection=tf.contrib.layers.l2_regularizer(lamda)())# 计算loss（已正则化）
     train_op = tf.train.AdamOptimizer(LR).minimize(loss) # 建立网络中训练的节点并利用Adam算法进行最优化，即最小化loss，LR为learning rate
     
     accuracy = tf.metrics.accuracy(labels=tf.argmax(label, axis=1), predictions=tf.argmax(output, axis=1), )[1] # 计算accuracy
@@ -82,14 +83,14 @@ def net(onehots_shape): #[73,398]
         
 ## Model Training ##
 
-def train_model(lr_ = LR):
+def train_model(BatchSize = BATCHSIZE, lr_ = LR):
 
     tf.reset_default_graph() # tf.reset_default_graph函数用于清除默认图形堆栈并重置全局默认图形。 
 
     train_data, train_label = load_data('train')
     valid_data, valid_label = load_data('validation')
     test_data, test_label = load_data('test')
-    init_op, train_op, loss, accuracy = net(train_data.shape[1:]) # [1:]表示从第一位开始索引，默认到最后一位，否则需要指定
+    init_op, train_op, loss, accuracy = net(train_data.shape[1:], 0.5) # [1:]表示从第一位开始索引，默认到最后一位，否则需要指定
     sess = tf.Session() # Session 是 Tensorflow 为了控制,和输出文件的执行的语句. 运行 session.run() 可以获得你要得知的运算结果, 或者是你所要运算的部分.
     sess.run(init_op) # 运行传递来的init_op，初始化全局和局部变量
     saver = tf.train.Saver()
@@ -97,6 +98,7 @@ def train_model(lr_ = LR):
     print("Training...")
 
     train_size = train_data.shape[0]
+    accuracyArr = []
     for epoch in range(EPOCH):
         time1 = 0
         if epoch < 1:
@@ -111,27 +113,65 @@ def train_model(lr_ = LR):
             for i in range(0, valid_data.shape[0], BatchSize):
                 b_data, b_label = valid_data[i: i+BatchSize], valid_label[i: i+BatchSize]
                 accuracy_ += sess.run(accuracy, {'input:0': b_data, 'label:0': b_label})
-            accuracy_ = accuracy_ * BatchSize / valid_data.shape[0]
-            print('Epoch:', epoch, '| Train Loss: %.4f' % loss_, 'Train Accuracy: %.2f' % accuracy_)
+                #print (accuracy_)
+            accuracy_ = accuracy_ * BatchSize / (valid_data.shape[0] + BatchSize) # 为了将train accuracy的均值限制在[0, 1]之间
+            print('Epoch:', epoch, '| Train Loss: %.4f' % loss_, 'Current Validation Accuracy: %.4f' % accuracy_)
+            accuracyArr.append(accuracy_)
         
         if epoch < 1:
             dt = round(time.time() - time1, 2)
             print("Epoch Time(sec): ", dt)
             print("Estimated Time(min): ", round(dt * EPOCH / 60, 1))
     
-        if epoch % 10 == 0:
+        if epoch % 10 == 0: 
             lr_ = lr_ / 1.2
         
     accuracy_ = 0
     b_data, b_label = valid_data, valid_label
     accuracy_ = sess.run(accuracy, {'input:0': b_data, 'label:0': b_label})
 
-    print("Validation Accuracy: ", accuracy_)
+    print("Validation Accuracy: %.4f", accuracy_)
         
     saver.save(sess, './weights/model')
     sess.close()
+    
+    return accuracy_, accuracyArr
         
-train_model()
+
+        
+        
+## Train Model ##
+
+#(accuracy, accuracyArr) = train_model(BATCHSIZE, LR)
+
+
+# 以下部分是为了局部最优化BatchSize
+
+max_accuracy = 0
+max_variance = -100000
+curRound = 0
+for size in range (64, 128, 16):
+    curRound = curRound + 1
+    print ("------------------------")
+    print ('Round: ', curRound, '| BatchSize = ', size)
+    (accuracy, accuracyArr) = train_model(size, LR)
+        
+    if accuracy > max_accuracy :
+        max_accuracy = accuracy
+        size1 = size
+            
+    relativeAccuracySum = 0.00
+    for i in range (0, accuracyArr.size() - 2, 1):
+        if accuracyArr[i + 1] > accuracyArr[i]:
+            relativeAccuracySum += pow(accuracyArr[i + 1] - accuracyArr[i], 2)
+        else:
+            relativeAccuracySum -= pow(accuracyArr[i] - accuracyArr[i + 1], 2)
+    variance = float(relativeAccuracySum / float(accuracyArr.size()))
+    if variance > min_variance:
+        min_variance = variance
+        size2 = size
+print ('Max Accuracy: ', max_accuracy, 'BatchSize: ', size1)
+print ('Max Variance: ', max_variance, 'BatchSize: ', size2)
         
         
 
